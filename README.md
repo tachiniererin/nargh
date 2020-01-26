db635af267647d43dbf64d5cc4b095d7a6b07ec7
Elen Eisendle <ln@calcifer.ee>
2020-01-26 21:56:48 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,35 @@
+# NARGH, a LCSC scraper
+
+This scrapes LCSC, because i have ADD and can't finish the firmware for my boards.
+
+That's all.
+
+## Usage
+
+Please use the pre-processed data. It's being updated semi-regularly and (not-yet) hosted [here](https://notweb.site/lcsc.tar.zst).
+
+To install it into `$GOPATH/bin` run:
+```sh
+go get github.com/tachiniererin/nargh
+go install github.com/tachiniererin/nargh
+```
+
+Otherwise you can just clone the repo and run it with `go run nargh.go`.
+
+
+## Product categories
+
+Look for the string `分类数据` in the page source of [`https://lcsc.com/products`](https://lcsc.com/products).
+This contains all the categories as a list. The file [`categories.json`](categories.json) contains
+this data, just formatted nicely.
+
+## Why is it called Nargh?
+
+Because that was the sound I made when I realised what I did.
+
+## TODO
+
+- Product Pictures
+- Datasheets
+- Scrape JLCSMT part library (which was the initial goal)
+- Make the terminal output a bit prettier.
---
+++ nargh.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/cretz/bine/tor"
+)
+
+var (
+	torInstance *tor.Tor
+)
+
+type SubCategory struct {
+	ID         int
+	Name       string
+	URL        string
+	ProductNum int `json:"product_num"`
+}
+
+type Category struct {
+	SubCategory
+	Subs []SubCategory `json:",omitempty"`
+}
+
+type SearchResult struct {
+	Success bool
+	Message string
+	Code    int
+	Result  struct {
+		Data             []map[string]interface{}
+		CurrentPage      string `json:"current_page"`
+		LastPage         int    `json:"last_page"`
+		TotalPage        int    `json:"total_page"`
+		Total            int
+		RecommendKeyword interface{} `json:"recommendKeyword"`
+		Msg              interface{}
+	}
+}
+
+func newCircuit() error {
+	// close the remaining connections and build a new identity
+	http.DefaultClient.CloseIdleConnections()
+	return torInstance.Control.Signal("NEWNYM")
+}
+
+func searchProducts(page int, category int) (sr SearchResult, err error) {
+	args := url.Values{}
+	args.Set("current_page", strconv.Itoa(page))
+	args.Set("category", strconv.Itoa(category))
+	args.Set("in_stock", "false")
+	args.Set("is_RoHS", "false")
+	args.Set("show_icon", "false")
+	args.Set("search_content", "")
+
+	resp, err := http.PostForm("https://lcsc.com/api/products/search", args)
+
+	if err != nil {
+		return
+	}
+	if err = json.NewDecoder(resp.Body).Decode(&sr); err != nil {
+		return
+	}
+
+	return
+}
+
+func dlSub(c SubCategory) {
+	var partsu []map[string]interface{}
+
+	// scrape the first page of the category
+	// this gives us the number of product pages
+	sr, err := searchProducts(1, c.ID)
+	if err != nil {
+		log.Printf("Category %d:%d\n", c.ID, 1)
+		log.Printf("Error: %v\n", err)
+		return
+	}
+
+	// get a new identity if we get told off
+	if !sr.Success {
+		log.Printf("Server told us to back off, creating a new circuit")
+		if err := newCircuit(); err != nil {
+			log.Fatalf("Failed to create a new circuit: %v", err)
+		}
+	}
+
+	log.Printf("Fetching subcategory %s\t(%d) with %d pages\n", c.Name, c.ID, sr.Result.TotalPage)
+
+	// append new parts
+	partsu = append(partsu, sr.Result.Data...)
+
+	// go through the pages and collect all the data
+	for page := 2; page < sr.Result.LastPage; page++ {
+		sr, err := searchProducts(page, c.ID)
+		if err != nil {
+			log.Printf("Category %d:%d\n", c.ID, page)
+			log.Printf("Error: %v\n", err)
+			return
+		}
+
+		if !sr.Success {
+			log.Printf("Server told us to back off, creating a new circuit")
+			page--
+			if err := newCircuit(); err != nil {
+				log.Fatalf("Failed to create a new circuit: %v", err)
+			}
+			continue
+		}
+
+		// append new parts
+		partsu = append(partsu, sr.Result.Data...)
+	}
+
+	// write the data nicely indented to disk
+	f, err := os.Create(fmt.Sprintf("json/%d.json", c.ID))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	enc := json.NewEncoder(f)
+	enc.SetIndent("", "    ")
+	if err := enc.Encode(partsu); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	var categories []Category
+
+	log.Println(`
+    _____ _____ _____ _____ _____ 
+   |   | |  _  | __  |   __|  |  |
+   | | | |     |    -|  |  |     |
+   |_|___|__|__|__|__|_____|__|__|
+   Your friendly LCSC scraper.
+   `)
+
+	// load categories, these are from the JS in https://lcsc.com/products
+	// manually update if needed
+	b, err := ioutil.ReadFile("categories.json")
+	if err != nil {
+		log.Fatalf("Could not load categories.json (%v)\n", err)
+	}
+
+	if err := json.Unmarshal(b, &categories); err != nil {
+		log.Fatalf("Check categories.json for errors (%v)\n", err)
+	}
+
+	// start TOR so that we can scrape it faster :3
+	log.Println("Starting TOR...")
+	torInstance, err = tor.Start(context.TODO(), nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer torInstance.Close()
+
+	// wait at most a minute to start tor
+	dialCtx, dialCancel := context.WithTimeout(context.Background(), time.Minute)
+	defer dialCancel()
+
+	// set up the connection to the proxy
+	dialer, err := torInstance.Dialer(dialCtx, nil)
+	if err != nil {
+		log.Fatalf("Could not establish TOR Dialer: %v", err)
+	}
+
+	// set up the default http client with our tor proxy
+	http.DefaultClient = &http.Client{Transport: &http.Transport{DialContext: dialer.DialContext}}
+
+	for _, c := range categories {
+		log.Printf("Fetching category %s (%d)\n", c.Name, c.ID)
+		for _, sc := range c.Subs {
+			dlSub(sc)
+		}
+	}
+}
